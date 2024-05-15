90401be8361381f8ba0b5037b621677ec1ca38c4
IllFil <152105364+IllFil@users.noreply.github.com>
2024-05-15 19:54:25 +0300

    Added feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class UpdateOwnProfile(permissions.BasePermission):
-    """Allow users to edit their own profile"""
+    """Allow user to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
@@ -10,6 +10,7 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
 
 class UpdateOwnStatus(permissions.BasePermission):
     """Allow users to update their own status"""
